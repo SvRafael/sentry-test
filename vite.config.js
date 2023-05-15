22f10a8c5164bab0967909f4c5398d53d55feec6
Rafael Souza <rafaelsvsouza@hotmail.com>
2023-05-15 10:20:28 -0300

    my code ir perfect

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,9 @@ const viteSentryConfig = {
   org: "cygni",
   project: "cygni-ag-teste",
   release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
-  setCommits: {},
+  setCommits: {
+    auto: true,
+  },
   sourceMaps: {
     include: ["./dist/assets"],
     ignore: ["node_modules"],
