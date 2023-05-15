e03edff630e9cab06b49928368ea3a8461834b26
Rafael Souza <rafaelsvsouza@hotmail.com>
2023-05-15 10:29:14 -0300

    my code ir perfect

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="text-4xl">About</h1>
-    <button @click="user.email.send()">Send Email</button>
+    <button @click="a()">Send Email</button>
   </div>
 </template>
 
