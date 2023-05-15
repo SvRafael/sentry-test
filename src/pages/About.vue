7922ae085f1f00b887cbb0183515603bf4e77d8d
Rafael Souza <rafaelsvsouza@hotmail.com>
2023-05-15 11:13:37 -0300

    teste

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="text-4xl">About</h1>
-    <button @click="user.a.send()">Send Email</button>
+    <button @click="user.user.send()">Send Email</button>
   </div>
 </template>
 
